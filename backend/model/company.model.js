639521bbadfdad0ca1f04ca7ef2c0f7c38b7a511
Vishvadeep <vishvadeep.0001@example.com>
2025-11-15 11:37:16 +0530

    Company model created

---
+++ backend/model/company.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    wesbite: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+    },
+    logo: {
+      type: String,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Company = mongoose.model("Company", companySchema);
