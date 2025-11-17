a8dccb0f939e93642261d758bc81b499ee068a3f
Vishvadeep <vishvadeep.0001@example.com>
2025-11-17 22:46:56 +0530

    Company controller created and started with registercompany, getCompany, getCompanyById

---
+++ backend/controllers/company.controller.js
@@ -0,0 +1,73 @@
+import { Company } from "../model/company.model.js";
+
+export const registerCompany = async (req, res) => {
+  try {
+    const { companyName } = req.body;
+
+    if (!companyName) {
+      return res.status(400).json({
+        message: "Company name is required.",
+        success: false,
+      });
+    }
+
+    let company = await Company.findOne({ name: companyName });
+
+    if (company) {
+      return res.status(400).json({
+        message: "You can't regsiter same company",
+        success: false,
+      });
+    }
+    company = await Company.create({
+      name: companyName,
+      userId: req.id,
+    });
+
+    return res.status(201).json({
+      message: "Company registered successfully.",
+      company,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCompany = async (req, res) => {
+  try {
+    const userId = req.id; // Logged in user
+    const companies = await Company.find({ userId });
+
+    if (!companies) {
+      return res.status(404).json({
+        message: "Companies not found !",
+        success: false,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Get company by id
+
+export const getCompanyById = async (req, res) => {
+  try {
+    const companyId = req.params.id;
+    const company = await Company.findById(companyId);
+    if (!company) {
+      return res.status(404).json({
+        message: "Company not found ! ",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      company,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
