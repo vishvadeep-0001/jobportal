3195cc5c82dc1577cdede3dab7263c15af00f84e
Vishvadeep <vishvadeep.0001@example.com>
2025-11-28 23:16:50 +0530

    Home page ui completed with all css.

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
     <div className="text-center">
       <div className="flex flex-col gap-5 my-2">
         <span className="mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium">
-          No. 1 Job Hunt Website
+          No. 1 Job Portal Website
         </span>
         <h1 className="text-5xl font-bold">
           Search, Apply & <br />
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,14 +1,17 @@
 import Navbar from "./shared/Navbar";
 import HeroSection from "./HeroSection";
+import CategoryCarousel from "./CategoryCarousel";
+import LatestJobs from "./LatestJobs.jsx";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
-      {/* <CategoryCarousel />
+      <CategoryCarousel />
       <LatestJobs />
-      <Footer /> */}
+      <Footer />
     </>
   );
 };
