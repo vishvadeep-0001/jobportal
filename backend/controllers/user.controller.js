02698a0c75cf15dd890cb743a0904e359cb90fb3
Vishvadeep <98748499+vishvadeep-0001@users.noreply.github.com>
2026-02-22 17:14:49 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -105,7 +105,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpsOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
+          secure: true,
       })
       .json({
         message: `Welcome back ${user.fullname}`,
